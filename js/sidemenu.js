e42576aed4354a67fa60d739c6570c7fb49eea9f
Søren Ovesen <eaasbo@students.eaaa.dk>
2019-04-02 12:45:49 +0200

    Responsivitet til mobil

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,6 +1,33 @@
 var overskrifter = $(".sidemenu_tekst:odd");
+var knap = $(".burger_knap");
+var menu = $(".sidemenu_mobil");
 
 overskrifter.css(
+{
+    "color": "#f5a40f",
+});
+
+knap.click(function()
+{
+    if (menu.width() == 0)
     {
-        "color": "#f5a40f",
-    });
+        menu.animate(
+            {
+                width: "100%"
+            }
+        )
+    }
+    else
+    {
+        menu.animate(
+            {
+                width: "0"
+            }
+        )
+    }
+});
+
+$(window).resize(function()
+{
+    $(".sidemenu_mobil:hidden").width(0);
+})
