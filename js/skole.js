56cb45f981f798bfd99322266f56e747aed9cf96
Søren Ovesen <eaasbo@students.eaaa.dk>
2019-04-03 11:49:39 +0200

    Navigationspile, mindre billeder

--- js/skole.js
+++ js/skole.js
@@ -2,6 +2,7 @@ var upArrows = $("#pilOp");
 var downArrows = $("#pilNed");
 var leftArrows = $("#pilVenstre");
 var rightArrows = $("#pilHøjre");
+var allArrows = $("#pilOp, #pilNed, #pilVenstre, #pilHøjre");
 var images = $(".mapimg");
 //var images = $("#test");
 var container = $(".skole_navigator");
@@ -32,6 +33,8 @@ $().ready(function()
             top: 100 * (startX + targetY) + "%"
         }
     )
+
+    clickArrow();
 })
 
 upArrows.click(function() 
@@ -64,7 +67,62 @@ rightArrows.click(function()
 
 function clickArrow()
 {
-    $("#test").text($("#test").offset().left + "," + $("#test").offset().top + " ; " + targetX + "," + targetY);
+    //$("#test").text($("#test").offset().left + "," + $("#test").offset().top + " ; " + targetX + "," + targetY);
+
+    allArrows.hide();
+
+    if (targetY == 0)
+    {
+        downArrows.css({top: "70%", left: "50%"});
+
+        downArrows.show();
+    }
+
+    if (targetY == -1)
+    {
+        if (targetX == 0)
+        {
+            downArrows.css({top: "55%", left: "70%"});
+            rightArrows.css({top: "50%", left: "90%"});
+            upArrows.css({top: "35%", left: "45%"});
+    
+            downArrows.show();
+            rightArrows.show();
+            upArrows.show();
+        }
+        else
+        {
+            leftArrows.css({top: "50%", left: "10%"});
+            leftArrows.show();
+        }
+    }
+
+    if (targetY == -2)
+    {
+        downArrows.css({top: "35%", left: "70%"});
+        upArrows.css({top: "75%", left: "65%"});
+
+        upArrows.show();
+        downArrows.show();
+    }
+
+    if (targetY == -3)
+    {
+        if (targetX == 0)
+        {
+            upArrows.css({top: "50%", left: "10%"});
+            rightArrows.css({top: "50%", left: "90%"});
+
+            upArrows.show();
+            rightArrows.show();
+        }
+        else
+        {
+            leftArrows.css({top: "50%", left: "10%"});
+
+            leftArrows.show();
+        }
+    }
 
     images.each(function()
     {
