367d919a30bf6732c02ff230ba577e6c93e072ee
Søren Ovesen <eaasbo@students.eaaa.dk>
2019-04-05 09:48:55 +0200

    Kommentarer og justering af pile

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -2,13 +2,16 @@ var overskrifter = $(".sidemenu_tekst:odd");
 var knap = $(".burger_knap");
 var menu = $(".sidemenu_mobil");
 
+// Gør hver anden knap i menuen gul
 overskrifter.css(
 {
     "color": "#f5a40f",
 });
 
+// Der trykkes på burgermenu-knappen
 knap.click(function()
 {
+    // Hvis bredden er 0, animér bredden op til 100%
     if (menu.width() == 0)
     {
         menu.animate(
@@ -17,7 +20,7 @@ knap.click(function()
             }
         )
     }
-    else
+    else // Ellers, animér den ned til 0
     {
         menu.animate(
             {
@@ -27,6 +30,7 @@ knap.click(function()
     }
 });
 
+// Når vinduet ændrer størrelse, sæt bredden på den skjulte mobilmenu til 0
 $(window).resize(function()
 {
     $(".sidemenu_mobil:hidden").width(0);
--- js/skole.js
+++ js/skole.js
@@ -20,6 +20,7 @@ var maxHeight = $(".mapimg:first").height();
 
 $().ready(function()
 {
+    // Find højden af det højeste billede
     images.each(function()
     {
         if ($(this).height() > maxHeight)
@@ -28,8 +29,10 @@ $().ready(function()
         }
     })
 
+    // Sæt navigationskassens højde til at være lig med højden af det første billede
     container.height(maxHeight);
 
+    // Indstil billedernes startposition
     images.css(
         {
             left: 100 * (startX + targetX) + "%",
@@ -40,13 +43,17 @@ $().ready(function()
     clickArrow();
 })
 
+// Pil op klikkes
 upArrows.click(function() 
 {
+    // Incrementér positionsvariablen; angives i "hele bredder" af navigationsvinduet
     targetY += 1;
-        
+    
+    // Kør clickArrow funktionen
     clickArrow();
 });
 
+// Pil ned klikkes
 downArrows.click(function() 
 {
     targetY -= 1;
@@ -54,6 +61,7 @@ downArrows.click(function()
     clickArrow();
 });
 
+// Pil venstre klikkes
 leftArrows.click(function() 
 {
     targetX += 1;
@@ -61,6 +69,7 @@ leftArrows.click(function()
     clickArrow();
 });
 
+// Pil højre klikkes
 rightArrows.click(function() 
 {
     targetX -= 1;
@@ -68,24 +77,34 @@ rightArrows.click(function()
     clickArrow();
 });
 
+// Generel funktion til når en pil klikkes
 function clickArrow()
 {
+    // Debug ting til at vise information visuelt
     //$("#test").text($("#test").offset().left + "," + $("#test").offset().top + " ; " + targetX + "," + targetY);
 
+    // Reset alle pilene så vi ikke hele tiden skal gøre det manuelt
     allArrows.hide();
+    allArrows.css("transform", "rotate(0deg) translateX(-50%) translateY(-50%)");
 
+    // Første sal
     if (targetY == 0)
     {
+        // Sæt teksten af infoboksen
         var infoText = "<h1>Første sal</h1>";
 
+        // Sæt pilen(es) position
         downArrows.css({top: "70%", left: "50%"});
 
+        // Vis de relevante pile
         downArrows.show();
     }
 
+    // Stueetage
     if (targetY == -1)
     {
-        if (targetX == 0)
+        // Der er flere horisontale billeder her, som rykkes med targetX
+        if (targetX == 0)   // Forhal
         {
             var infoText = "<h1>Forhallen</h1><p>Dette er synet der møder dig når du træder ind i Guldtanden. Til venstre er der reception og lærerværelse, oppe ad trapperne ligger de fleste klasseværelser, nedad ligger atriummet og kantinen, til højre et par klasseværelser, og bag dig er der borde, stole og sofaer til fri afbenyttelse.<p>";
 
@@ -97,7 +116,7 @@ function clickArrow()
             rightArrows.show();
             upArrows.show();
         }
-        else
+        else    // Bag forhallen
         {
             var infoText = "<h1>Bag forhallen</h1>";
 
@@ -106,9 +125,14 @@ function clickArrow()
         }
     }
 
+    // Atriummet
     if (targetY == -2)
     {
         var infoText = "<h1>Atriummet</h1>";
+
+        // Rotér pile for mere intuitivt udseende
+        downArrows.css("transform", "rotate(180deg) translateX(50%) translateY(50%)");
+        upArrows.css("transform", "rotate(180deg) translateX(50%) translateY(50%)");
 
         downArrows.css({top: "35%", left: "70%"});
         upArrows.css({top: "75%", left: "65%"});
@@ -117,9 +141,10 @@ function clickArrow()
         downArrows.show();
     }
 
+    // Kælderetagen
     if (targetY == -3)
     {
-        if (targetX == 0)
+        if (targetX == 0)   // Kantinen
         {
             var infoText = "<h1>Kantinen</h1>";
 
@@ -129,7 +154,7 @@ function clickArrow()
             upArrows.show();
             rightArrows.show();
         }
-        else
+        else    // Spisesalen
         {
             var infoText = "<h1>Spisesalen</h1>";
 
@@ -139,6 +164,7 @@ function clickArrow()
         }
     }
 
+    // Animér alle billederne til deres nye position
     images.each(function()
     {
         $(this).animate(
@@ -148,17 +174,23 @@ function clickArrow()
         });
     })
 
+    // Put den nye info tekst ind i informationsboksen
     infoBox.html(infoText);
 }
 
+// Der klikkes på spørsmålstegnet, og informationsboksen vises eller skjules
 infoButton.click(function()
 {
     infoBox.toggle("fast");
 });
 
+// Kaldes når vinduet ændrer størrelse, for at navigationsboksen skal fungere ordenligt på alle størrelser
 $(window).resize(function()
 {
+    // Nulstil variablen for højeste billede
     maxHeight = 0;
+
+    // Flyt alle billeder til den rigtige position, og find højden af det største billede igen
     images.each(function()
     {
         $(this).css(
@@ -173,5 +205,6 @@ $(window).resize(function()
         }
     })
 
+    // Indstil højden på navigationselementet
     container.height(maxHeight);
 });
