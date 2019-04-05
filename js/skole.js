2857f2186a46f55710ce640048cf5ceb33cca9b7
Søren Ovesen <eaasbo@students.eaaa.dk>
2019-04-05 12:03:55 +0200

    Content til navigator

--- js/skole.js
+++ js/skole.js
@@ -85,13 +85,13 @@ function clickArrow()
 
     // Reset alle pilene så vi ikke hele tiden skal gøre det manuelt
     allArrows.hide();
-    allArrows.css("transform", "rotate(0deg) translateX(-50%) translateY(-50%)");
+    allArrows.css("transform", "translateX(-50%) translateY(-50%) rotate(0deg)");
 
     // Første sal
     if (targetY == 0)
     {
         // Sæt teksten af infoboksen
-        var infoText = "<h1>Første sal</h1>";
+        var infoText = "<h1>Første sal</h1><p>Skolen tilbyder flere områder for de studerende at kunne arbejde i grupper. Heraf ser vi sofaer, gruppeborde og generelle områder så er sat op, som optimale forhold for de studerende. Disse studieområder er sat nær klasseværelserne.</p>";
 
         // Sæt pilen(es) position
         downArrows.css({top: "70%", left: "50%"});
@@ -118,7 +118,7 @@ function clickArrow()
         }
         else    // Bag forhallen
         {
-            var infoText = "<h1>Bag forhallen</h1>";
+            var infoText = "<h1>Bag forhallen</h1><p>Bag forhallen findes et område med borde, stole og sofaer. Her kan man bruge tid på at lave lektier, skrive projekter eller bare slappe af. Her vil studievejlederen også være to gange om ugen, henholdsvis mandag og onsdag. Man kan bestille en tid, men man kan også bare møde op og få en snak omkring uddannelsen.</p>";
 
             leftArrows.css({top: "50%", left: "10%"});
             leftArrows.show();
@@ -128,11 +128,11 @@ function clickArrow()
     // Atriummet
     if (targetY == -2)
     {
-        var infoText = "<h1>Atriummet</h1>";
+        var infoText = "<h1>Atriummet</h1><p>I atriummet er der mulighed for at sidde i grupper og arbejde, spise frokost eller bare holde en pause efter undervisningen. Om torsdagen spilles der Kahoot som alle kan deltage i med gevinst til vinderen. Ind imellem afholdes der også oplæg og små messer med gæster udefra i atriummet.</p>";
 
         // Rotér pile for mere intuitivt udseende
-        downArrows.css("transform", "rotate(180deg) translateX(50%) translateY(50%)");
-        upArrows.css("transform", "rotate(180deg) translateX(50%) translateY(50%)");
+        downArrows.css("transform", "translateX(-50%) translateY(-50%) rotate(180deg)");
+        upArrows.css("transform", "translateX(-50%) translateY(-50%) rotate(180deg)");
 
         downArrows.css({top: "35%", left: "70%"});
         upArrows.css({top: "75%", left: "65%"});
@@ -146,7 +146,7 @@ function clickArrow()
     {
         if (targetX == 0)   // Kantinen
         {
-            var infoText = "<h1>Kantinen</h1>";
+            var infoText = "<h1>Kantinen</h1><p>I vores kantine tilbydes et bred udvalg af både varme og lune retter. Heraf finder du en lækker bland-selv salat buffet bestående af friske råvarer, forskellige slags sandwich og flere forskellige slags varme retter, som ændres fra dag til dag.</p>";
 
             upArrows.css({top: "50%", left: "10%"});
             rightArrows.css({top: "50%", left: "90%"});
@@ -156,7 +156,7 @@ function clickArrow()
         }
         else    // Spisesalen
         {
-            var infoText = "<h1>Spisesalen</h1>";
+            var infoText = "<h1>Spisesalen</h1><p>I spisesalen kan du nyde et måltid, enten ved at tage det købte måltid fra kantinen eller din egen medbragte madpakke. I kantinen finder du også studerende, som enten arbejder i gruppe eller have en god tid.<br>Ved specielle lejligheder afholdes messer og oplæg her.</p>";
 
             leftArrows.css({top: "50%", left: "10%"});
 
